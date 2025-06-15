acbae45618695b0e69d1d740d804c248900258ef
Nabina <sabinanabina@gmail.com>
2025-06-15 08:30:52 +0545

    all files added

---
+++ hello.py
@@ -0,0 +1,24 @@
+# def greet(name):
+#     return f"Hello, {name}"
+
+# from greet import greet
+
+# # print(greet("Ram"))
+# #print(greet
+
+# print(greet.greet("Ram"))
+# print(greet.greet("Hari"))
+
+
+
+import greet as gt
+
+# print(gt.greet("Ram"))
+# print(gt.greet("Hari"))
+
+
+if __name__=="__main__":
+    print(gt.greet("Gopal"))
+
+if __name__=="__main__":
+  print(gt.greet("maina")) 
